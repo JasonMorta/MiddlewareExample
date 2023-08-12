99fd3217b38cdc8f4e3eb5686ef61369930cdb42
Jason M <46291780+JasonMorta@users.noreply.github.com>
2023-08-12 15:56:52 +0200

    updates

--- routes/myLoggerRoute.js
+++ routes/myLoggerRoute.js
@@ -1,28 +1,29 @@
 
 //myLoggerRoute.js 
-// Step 3: Import the middleware functions and controller functions
+// Step 2: Import the middleware functions and controller functions
 // You must destructured the imported functions using {}.
 const { loggerMiddleware } = require('../middleware/myLogger');
 const { loggerController } = require('../controllers/loggerController');
 
-//Step 2:  Define route function
+//Step 3:  Define route function
 const myLoggerRoute = (app) => {
   // 'app' parameter is the express app object.
 
   /* 
-  When a GET request is made to the '/', 
-  the 'loggerMiddleware' middleware function will be executed first, 
-  followed by the 'loggerController' controller function.
-  NOTE: The middleware MUST to be placed before the controller function.
-
-  More middleware functions and controller functions can be added to the route, 
-  but they must be placed in the correct order.
+  When a GET request is made to the 'http://localhost:8080/',   
+  the 'loggerMiddleware' middleware function will be executed first,   
+  followed by the 'loggerController' controller function.  
+  
+  NOTE ⚠: The middleware MUST be placed before the controller functions.  
+  More middleware functions and controller functions can be added to the route,   
+  but they must be placed in the correct order.  
   For example: app.get('/', middleware1, middleware2, controller1, ect...)
   */
 
   
   app.get('/', loggerMiddleware, loggerController);
+  // This route URL will be http://localhost:8080/
 };
 
-//export the myLoggerRoute function to be used in App.js
+//export the myLoggerRoute function to be used in "../app.js"
 module.exports = myLoggerRoute;
