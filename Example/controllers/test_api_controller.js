926145c1c9317de23f422685fd0a3e229bcf8b1e
Jason M <46291780+JasonMorta@users.noreply.github.com>
2024-01-24 21:38:57 +0200

    Update test_api_controller.js

--- Example/controllers/test_api_controller.js
+++ Example/controllers/test_api_controller.js
@@ -1,16 +1,19 @@
 //test API
 const test_api_ctrl = (req, res) => {
         // Log headers
-        console.log('Headers:', req.headers);
+        console.log('req:', req);
 
         // Log body data
         if (req.method === 'POST' || req.method === 'PUT') {
             console.log('Body:', req.body);
         }
 
+        // get the payload
+        
+
 
         
-    //res.send(req.body);
+    res.send({"status": "ok", "id": 1091983});
 }
 
 
